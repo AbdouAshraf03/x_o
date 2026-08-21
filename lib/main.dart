cef03d8d7dceb0cb2b4a7f22c4cd5437ef8c99f8
Abd-Elrahman Ashraf <abdouashrat123@gmail.com>
2026-08-21 17:10:29 +0300

    Add initial project structure and remove unused main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:easy_localization/easy_localization.dart';
-import 'core/routes/app_router.dart';
-import 'core/routes/routes.dart';
-import 'core/theme/app_theme.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Set preferred orientations
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
-
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.dark,
-    ),
-  );
-
-  runApp(MyApp());
+void main() {
+  runApp(const MyGame());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyGame extends StatelessWidget {
+  const MyGame({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'X-O',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
-      initialRoute: Routes.home,
-      onGenerateRoute: AppRouter.generateRoute,
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-    );
+    return MaterialApp();
   }
 }
